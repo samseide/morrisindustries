817b052bb469bc9658bfd0615c00c6d5d9c318ca
Sam Seide <sam.seide@mediacurrent.com>
2019-10-15 08:03:55 -0500

    updating to components

---
+++ src/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Find your ride photo online at twitter.com/MorrisHalloween</h3>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/RideRules/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const RideRules = () => {
+  return (
+    <div class="Ride-Rules">
+      <h2>
+        Morris Industries<br></br>Ghost Capture
+          </h2>
+      <p>
+        Morris Industries Ghost Capture is a slow motion thrill ride that includes lasers, smoke machines and a ride photo being taken and posted to twitter.
+          </p>
+      <div class="Warning-Heading">
+        WARNING!
+          </div>
+      <div class="Warning-Text">
+        <p>
+          For safety, you should be in good health and free from high blood pressure, heart, back or neck problems, motion sickness or other conditions that could be aggravated by this adventure.
+            </p>
+        <p>
+          Expectant mothers should not ride this ride.
+            </p>
+        <p>
+          Maximum ride weight limit is 250 pounds.
+            </p>
+        <p>
+          Supervise children at all times.
+            </p>
+      </div>
+    </div>
+  )
+}
+
+export default RideRules;
